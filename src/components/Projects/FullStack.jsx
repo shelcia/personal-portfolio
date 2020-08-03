919a24b10676c9952853909698eedeaa21ad4a18
Shelcia abi <shelciaabi@Shelcias-MacBook-Air-2320.local>
2020-08-03 20:29:19 +0530

    update

--- src/components/Projects/FullStack.jsx
+++ src/components/Projects/FullStack.jsx
@@ -1,29 +1,35 @@
 import React from "react";
+import CRM from "../../assets/crm.JPG";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const FullStack = ({ setView }) => {
   return (
     <React.Fragment>
       <div className="col-sm-2">
-        <div className="nav">
-          <p className="active">Full Stack</p>
-          <p onClick={() => setView("uidesign")}>UI Design</p>
-          <p onClick={() => setView("ongoing")}>On Going</p>
-          <p onClick={() => setView("design")}>Design</p>
-        </div>
+        <ScrollAnimation animateIn="fadeInLeft">
+          <div className="nav">
+            <p className="active">Full Stack</p>
+            <p onClick={() => setView("uidesign")}>UI Design</p>
+            <p onClick={() => setView("ongoing")}>On Going</p>
+            <p onClick={() => setView("design")}>Design</p>
+          </div>
+        </ScrollAnimation>
       </div>
       <div className="col-sm-10">
-        <div className="container">
-          <div className="row">
-            <div className="card">hi</div>
-            <div className="card">hi</div>
-            <div className="card">hi</div>
+        <ScrollAnimation animateIn="fadeInRight">
+          <div className="container">
+            <div className="row">
+              <div className="card">
+                <img src={CRM} alt="" />
+                <div className="text-container">
+                  <h2>CRM</h2>
+                  <p>Full stack Project based on CRM with NodeJS and React</p>
+                  <a href="https://github.com/shelcia/crm-frontend">Code</a>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="row">
-            <div className="card">hi</div>
-            <div className="card">hi</div>
-            <div className="card">hi</div>
-          </div>
-        </div>
+        </ScrollAnimation>
       </div>
     </React.Fragment>
   );
--- src/components/Projects/UIDesign.jsx
+++ src/components/Projects/UIDesign.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import Countries from "../../assets/countries.JPG";
+import Samgatha from "../../assets/samgatha.png";
+import ScrollAnimation from "react-animate-on-scroll";
 
-const UIDesign = () => {
-  return <React.Fragment>UIDesign</React.Fragment>;
+const UIDesign = ({ setView }) => {
+  return (
+    <React.Fragment>
+      <div className="col-sm-2">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <div className="nav">
+            <p onClick={() => setView("fullstack")}>Full Stack</p>
+            <p className="active">UI Design</p>
+            <p onClick={() => setView("ongoing")}>On Going</p>
+            <p onClick={() => setView("design")}>Design</p>
+          </div>
+        </ScrollAnimation>
+      </div>
+      <div className="col-sm-10">
+        <ScrollAnimation animateIn="fadeInRight">
+          <div className="container">
+            <div className="row">
+              <div className="card">
+                <img src={Countries} alt="" />
+                <div className="text-container">
+                  <h2>REST API Countrie</h2>
+                  <p>Project built using REST Countries API</p>
+                  <a href="https://github.com/shelcia/countries">Code</a>
+                </div>
+              </div>
+              <div className="card">
+                <img src={Samgatha} alt="" />
+                <div className="text-container">
+                  <h2>Samgatha</h2>
+                  <p>
+                    I was commisioned to build website for college cultural fest
+                  </p>
+                  <a href="https://github.com/shelcia/Samgatha-2020">Code</a>
+                  <a href="http://www.samgatha.live/">Deployment</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </ScrollAnimation>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default UIDesign;
