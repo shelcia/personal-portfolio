92a70183a3837179d8b02735c2b26dc30b834d78
shelcia <shelcia1999@gmail.com>
2020-12-30 01:29:16 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,35 @@
 import React from "react";
+import Particles from "react-particles-js";
 
 const Home = () => {
   return (
     <React.Fragment>
       <div className="container page-fade">
-        <h1>Home</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          aliquet, purus vitae eleifend tristique, lorem magna volutpat orci, et
-          vehicula erat erat nec elit. Aenean posuere nunc ac cursus facilisis.
-          Aenean vel porta turpis, ut iaculis justo.
-        </p>
+        <div className="d-relative" style={{ zIndex: 10000 }}>
+          <h1 className="d-relative">I am Shelcia</h1>
+          <h4>Web Developer and Designer</h4>
+        </div>
+
+        <Particles
+          params={{
+            particles: {
+              number: {
+                value: 50,
+              },
+              size: {
+                value: 3,
+              },
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+              },
+            },
+          }}
+        ></Particles>
       </div>
     </React.Fragment>
   );
