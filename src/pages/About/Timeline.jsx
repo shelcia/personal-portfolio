7c0e2877f453ea5ba0083b2f86e0ff1b7ed4c01a
shelcia <shelcia1999@gmail.com>
2020-12-29 11:11:50 +0530

    update

--- src/pages/About/Timeline.jsx
+++ src/pages/About/Timeline.jsx
@@ -14,90 +14,50 @@ const Timeline = () => {
           <h3 className="timeline__title">Born</h3>
         </div>
 
-        <h2 className="timeline__item timeline__item--year">2003</h2>
+        <h2 className="timeline__item timeline__item--year">2017</h2>
 
         <div className="timeline__item">
           <h3 className="timeline__title">Graduated High School</h3>
-        </div>
-
-        <h2 className="timeline__item timeline__item--year">2004</h2>
-
-        <div className="timeline__item">
-          <h3 className="timeline__title">
-            Started at Fox Valley Technical College (FVTC) in an Electrical
-            Engineering program
-          </h3>
-        </div>
-
-        <h2 className="timeline__item timeline__item--year">2006</h2>
-
-        <div className="timeline__item">
-          <h3 className="timeline__title">
-            Changed at FVTC to Web Design & Development program
-          </h3>
-        </div>
-
-        <h2 className="timeline__item timeline__item--year">2007</h2>
-
-        <div className="timeline__item">
-          <h3 className="timeline__title">Web Design Internship</h3>
-          <p className="timeline__blurb">started internship at company 1.</p>
-        </div>
-
-        <h2 className="timeline__item timeline__item--year">2008</h2>
-
-        <div className="timeline__item">
-          <h3 className="timeline__title">IT Internship</h3>
-          <p className="timeline__blurb">Started internship at company 2</p>
-        </div>
-
-        <div className="timeline__item">
-          <h3 className="timeline__title">Got Married</h3>
-        </div>
-
-        <div className="timeline__item">
-          <h3 className="timeline__title">Application Engineer</h3>
           <p className="timeline__blurb">
-            Started web design/dev job at company 2.
+            Graduated with 93.8% and scoring 95% in all PCM Subjects and
+            clearing JEE exams
           </p>
         </div>
 
-        <h2 className="timeline__item timeline__item--year">2012</h2>
+        <h2 className="timeline__item timeline__item--year">2019</h2>
 
         <div className="timeline__item">
-          <h3 className="timeline__title">First Child</h3>
-          <p className="timeline__blurb">
-            Spouse and I welcomed our first daughter.
-          </p>
-        </div>
-
-        <h2 className="timeline__item timeline__item--year">2015</h2>
-
-        <div className="timeline__item">
-          <h3 className="timeline__title">Second Child</h3>
-          <p className="timeline__blurb">
-            Spouse and I welcomed our second daughter.
-          </p>
-        </div>
-
-        <h2 className="timeline__item timeline__item--year">2016</h2>
-
-        <div className="timeline__item">
-          <h3 className="timeline__title">Lead Application Engineer</h3>
-          <p className="timeline__blurb">Promotion to mid-level role.</p>
-        </div>
-
-        <h2 className="timeline__item timeline__item--year">2018</h2>
-
-        <div className="timeline__item">
-          <h3 className="timeline__title">Aerial/Circus Arts</h3>
+          <h3 className="timeline__title">Academic Internship @ NUS</h3>
           <p className="timeline__blurb">
             Started taking classes to learn aerial silks, hoop, and hammock.
           </p>
         </div>
 
-        <h2 className="timeline__item timeline__item--year">2019</h2>
         <h2 className="timeline__item timeline__item--year">2020</h2>
+        <div className="timeline__item">
+          <h3 className="timeline__title">Academic Internship @ NUS</h3>
+          <p className="timeline__blurb">
+            Started taking classes to learn aerial silks, hoop, and hammock.
+          </p>
+        </div>
+        <div className="timeline__item">
+          <h3 className="timeline__title">Academic Internship @ NUS</h3>
+          <p className="timeline__blurb">
+            Started taking classes to learn aerial silks, hoop, and hammock.
+          </p>
+        </div>
+        <div className="timeline__item">
+          <h3 className="timeline__title">Academic Internship @ NUS</h3>
+          <p className="timeline__blurb">
+            Started taking classes to learn aerial silks, hoop, and hammock.
+          </p>
+        </div>
+        <div className="timeline__item">
+          <h3 className="timeline__title">Academic Internship @ NUS</h3>
+          <p className="timeline__blurb">
+            Started taking classes to learn aerial silks, hoop, and hammock.
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import PDFViewer from "pdf-viewer-reactjs";
 
 const Contact = () => {
   return (
     <React.Fragment>
       <div className="container page-fade">
-        <h1>Contact</h1>
+        <h1 className="glitch" data-text="Contact">
+          Contact
+        </h1>
         <p>
           Aliquam iaculis a nisi sed ornare. Sed venenatis tellus vel consequat
           congue. In bibendum vestibulum orci et feugiat.
         </p>
+        <PDFViewer
+          document={{
+            url: "https://arxiv.org/pdf/quant-ph/0410100.pdf",
+          }}
+        />
       </div>
     </React.Fragment>
   );
