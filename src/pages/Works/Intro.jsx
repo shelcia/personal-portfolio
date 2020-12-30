511b0cbf9a21472685b29b80b2798f4b21d5d26e
shelcia <shelcia1999@gmail.com>
2020-12-31 03:17:30 +0530

    update

--- src/pages/Works/Intro.jsx
+++ src/pages/Works/Intro.jsx
@@ -7,7 +7,10 @@ const Intro = () => {
         <div className="container pt-5">
           <div className="row">
             <div className="col-sm-6">
-              <h1>Shelcia</h1>
+              <h1 className="title">Shelcia David.</h1>
+              <div className="d-flex">
+                <i className="fab fa-behance-square" />
+              </div>
             </div>
             <div className="col-sm-6">
               <p>Full Stack Devleoper and Deisgmer</p>
