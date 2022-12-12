bbe2f04d301060e1e2515126098751dc4dece57a
shelcia <shelcia1999@gmail.com>
2022-12-13 00:08:38 +0530

    ✨ new setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CustomTypographyLevel from "./components/Intro";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <div className="text-green-500">kflkfl</div>
+      <CustomTypographyLevel />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between p-2">
+      <div>02</div>
+      <div>03</div>
+    </nav>
+  );
+};
+
+export default Navbar;
