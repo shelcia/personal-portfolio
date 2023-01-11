75fe7ad447cc9275e3d2fed4e68472f8ca614ab8
shelcia <shelcia1999@gmail.com>
2023-01-11 18:29:38 +0530

    🚀 ✨ vite migration

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/namespace
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
