3a0fab570a4b0f5df17faf855baa8bbbe1364c9f
shelcia <shelcia1999@gmail.com>
2024-04-16 02:36:39 -0500

    🚀 deploy fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "dist",
   images: {
     domains: ["images.unsplash.com", "ik.imagekit.io", "miro.medium.com"],
   },
--- src/pages/components/ArticleSection.tsx
+++ src/pages/components/ArticleSection.tsx
@@ -23,39 +23,40 @@ const ArticleSection = ({
         className
       )}
     >
-      {items.map((item, idx) => (
-        <div
-          key={item?.link}
-          className="relative group  block p-2 h-full w-full"
-          onMouseEnter={() => setHoveredIndex(idx)}
-          onMouseLeave={() => setHoveredIndex(null)}
-        >
-          <AnimatePresence>
-            {hoveredIndex === idx && (
-              <motion.span
-                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl"
-                layoutId="hoverBackground"
-                initial={{ opacity: 0 }}
-                animate={{
-                  opacity: 1,
-                  transition: { duration: 0.15 },
-                }}
-                exit={{
-                  opacity: 0,
-                  transition: { duration: 0.15, delay: 0.2 },
-                }}
-              />
-            )}
-          </AnimatePresence>
-          <ArticleCard
-            key={item.pubDate}
-            title={item.title}
-            description={item.content}
-            image={item.thumbnail}
-            link={item.link}
-          />
-        </div>
-      ))}
+      {items &&
+        items?.map((item, idx) => (
+          <div
+            key={item?.link}
+            className="relative group  block p-2 h-full w-full"
+            onMouseEnter={() => setHoveredIndex(idx)}
+            onMouseLeave={() => setHoveredIndex(null)}
+          >
+            <AnimatePresence>
+              {hoveredIndex === idx && (
+                <motion.span
+                  className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl"
+                  layoutId="hoverBackground"
+                  initial={{ opacity: 0 }}
+                  animate={{
+                    opacity: 1,
+                    transition: { duration: 0.15 },
+                  }}
+                  exit={{
+                    opacity: 0,
+                    transition: { duration: 0.15, delay: 0.2 },
+                  }}
+                />
+              )}
+            </AnimatePresence>
+            <ArticleCard
+              key={item.pubDate}
+              title={item.title}
+              description={item.content}
+              image={item.thumbnail}
+              link={item.link}
+            />
+          </div>
+        ))}
     </div>
   );
 };
