6f4a2e28f4fbeb4e86139c31a1dc56afd2d4e556
shelcia <shelcia1999@gmail.com>
2024-04-21 22:04:33 -0500

    🐛 deploy fix

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -3,5 +3,5 @@ import localFont from "@next/font/local";
 
 export const dmsans = DM_Sans({ subsets: ["latin"] });
 export const calsans = localFont({
-  src: "../assets/cal-sans/calSans-SemiBold.woff2",
+  src: "../assets/cal-sans/CalSans-SemiBold.woff2",
 });
